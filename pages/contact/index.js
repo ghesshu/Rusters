2d711186159f4898704f73cfabb4ef5da8a1399f
Hughes <hughestotimee@gmail.com>
2025-07-18 08:20:28 +0000

    New Update

---
+++ pages/contact/index.js
@@ -0,0 +1,26 @@
+
+import React, { Fragment } from 'react';
+import ContactSection from '../../components/ContactSection';
+import PageTitle from '../../components/pagetitle/PageTitle';
+import Header4 from '../../components/header4/Header4';
+import Scrollbar from '../../components/scrollbar/scrollbar'
+import Footer from '../../components/footer/Footer';
+import Header from '../../components/header/Header';
+
+const ContactPage = (props) => {
+
+    return (
+        <Fragment>
+            <div className='index_ico page_wrapper'>
+                <Header />
+                <main className="page_content">
+                    <PageTitle pageTitle={'Contact Us'} pagesub={'Home'} />
+                    <ContactSection />
+                </main>
+                <Footer />
+                <Scrollbar />
+            </div>
+        </Fragment>
+    )
+};
+export default ContactPage;
